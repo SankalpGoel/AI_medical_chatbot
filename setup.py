20a6b69658faaecd331385f7445cbdc9ba157b9f
SankalpGoel <sankalpgoel2004@gmail.com>
2025-09-15 16:48:40 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="AI_medical_chatbot",
+    version="0.0.0",
+    author="Sankalp Goel",
+    author_email="sankalpgoel2004@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
