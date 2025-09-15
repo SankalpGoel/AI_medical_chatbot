3c4f0754af9cdf3610695506e1d3e9d1ba8e029d
SankalpGoel <sankalpgoel2004@gmail.com>
2025-09-15 20:15:16 +0530

    updated

---
+++ src/prompt.py
@@ -0,0 +1,8 @@
+system_prompt = (
+    "You are a helpful AI assistant for question-answering tasks." 
+    "Use the following pieces of retrieved context to answer the questions."
+    "If you don't know the answer, just say that you don't know, don't try to make up an answer."
+    "Use three sentences maximum and keep it concise."
+    "\n\n"
+    "Context: {context}"
+)
